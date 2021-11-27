2a255f712012771ed9210a784cb2eeb77be0d59e
MagonBC <magonbc@home.local>
2021-11-27 15:49:17 +0100

    fix service name in the code

--- service-a/src/main/java/com/toomuch2learn/microservices/servicea/client/ServiceBClient.java
+++ service-a/src/main/java/com/toomuch2learn/microservices/servicea/client/ServiceBClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "serviceb")
+@FeignClient(name = "service-b")
 public interface ServiceBClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/greeting")
