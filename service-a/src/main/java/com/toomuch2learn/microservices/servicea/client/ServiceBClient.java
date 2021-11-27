34a13b8a17612efa77ee39bfb3ee1a7a4eecbbb7
MagonBC <magonbc@home.local>
2021-11-27 14:39:26 +0100

    use consul discovery

--- service-a/src/main/java/com/toomuch2learn/microservices/servicea/client/ServiceBClient.java
+++ service-a/src/main/java/com/toomuch2learn/microservices/servicea/client/ServiceBClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "serviceb", url = "${serviceb.url}")
+@FeignClient(value = "serviceb")
 public interface ServiceBClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/greeting")
--- service-a/src/main/java/com/toomuch2learn/microservices/servicea/client/ServiceCClient.java
+++ service-a/src/main/java/com/toomuch2learn/microservices/servicea/client/ServiceCClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "servicec", url = "${servicec.url}")
+@FeignClient(value = "servicec")
 public interface ServiceCClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/greeting")
